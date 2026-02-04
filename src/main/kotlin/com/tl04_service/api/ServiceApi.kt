f4376904b7bbf43cefda54a170c01fdd367f0970
hblab-cuongtq <cuongtq@hblab.vn>
2026-02-04 16:23:52 +0700

    change RequestMapping

--- src/main/kotlin/com/tl04_service/api/ServiceApi.kt
+++ src/main/kotlin/com/tl04_service/api/ServiceApi.kt
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/service")
+@RequestMapping("/service-b/service")
 class ServiceApi {
     @GetMapping
     fun connect(): ResponseEntity<String> = ResponseEntity.ok().body("connected service successfully")
